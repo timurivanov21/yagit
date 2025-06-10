97db1baae32b48308495a5cdfb7ffeef8c58bbe3
UnMelow <Unmelow@yandex.ru>
2025-06-11 01:22:54 +0300

    feat(frontend): add global API error handler with simple notifications

--- frontYagit/src/main.jsx
+++ frontYagit/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {NotificationsProvider} from "./components/NotificationsProvider"
+import {installFetchInterceptor} from "./lib/fetchInterceptor"
+
+
+installFetchInterceptor();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <NotificationsProvider><App/></NotificationsProvider>
+    </StrictMode>
 )
